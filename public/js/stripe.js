fa7555b63509932c35bb4d2a9079d3032a5c8fda
Mohanad Abu Sabha <eng.mohanadsabha@gmail.com>
2025-04-11 01:06:55 +0300

    Create Checkout session on Frontend

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+    'pk_test_51RBdcBQgVqt3Pq0Dq3Z8lIeDgd7EhrW7W5jamYoxwHsaZ3t3Xu0ynvfTLlRzbbqQtA07BoaVtfKlgjlXWwgw3Zz000zEzOeb7c',
+);
+
+export const bookTour = async (tourId) => {
+    try {
+        // 1) Get checkout session from API
+        const session = await axios(
+            `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`,
+        );
+        console.log(session);
+
+        // 2) Create checkout form + chanre credit card
+        await stripe.redirectToCheckout({
+            sessionId: session.data.session.id,
+        });
+    } catch (err) {
+        console.log(err);
+        showAlert('error', err);
+    }
+};
